dc8b1148ced403f286c7612b25cac375d8aa3f37
VishvaS1999 <gvishva2601@gmail.com>
2022-12-14 22:28:22 +0530

    Compare Two Lines And Check One Line is Equal, Greater Or Lessthan

--- src/com/LineComparision/CalculateLength.java
+++ src/com/LineComparision/CalculateLength.java
@@ -9,33 +9,50 @@ public class CalculateLength
     {
 
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the Number x1 : ");
+
+        System.out.println("Enter The Value Of x1:");
         x1 = sc.nextDouble();
-        System.out.println("Enter The Number y1 : ");
+
+        System.out.println("Enter The Value Of y1:");
         y1 = sc.nextDouble();
-        System.out.println("Enter the Number x2 : ");
+
+        System.out.println("Enter The Value Of x2:");
         x2 = sc.nextDouble();
-        System.out.println("Enter the Number y2 : ");
+
+        System.out.println("Enter The Value Of y2:");
         y2 = sc.nextDouble();
-        Double len = Math.sqrt((x2 - x1) * (x2 - x1)  +  (y2 - y1) * (y2 - y1));
-        System.out.println("Length of the given Line is : " + len + " units" );
 
-        System.out.println("Enter the Number x3 : ");
+        double len = Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+        System.out.println("Length Of Line Is :" + len);
+
+        System.out.println("Enter The Value Of x3:");
         x3 = sc.nextDouble();
-        System.out.println("Enter the Number y3 : ");
-        y3 = sc.nextDouble();
-        System.out.println("Enter the Number x4 : ");
-        x4 = sc.nextDouble();
-        System.out.println("Enter the Number y4 : ");
-        y4 = sc.nextDouble();
-        Double len1 = Math.sqrt((x4 - x3) * (x4 - x3)  +  (y4 - y3) * (y4 - y3));
-        System.out.println("Length of the given Line is : " + len1 + " units" );
 
-        //System.out.println(len.equals(len1));
-        if(len.equals(len1))
-            System.out.println("both lines are equal");
+        System.out.println("Enter The Value Of y3:");
+        y3 = sc.nextDouble();
+
+        System.out.println("Enter The Value Of x4:");
+        x4 = sc.nextDouble();
+
+        System.out.println("Enter The Value Of y4:");
+        y4 = sc.nextDouble();
+
+        double len1 = Math.sqrt((x4 - x3) * (x4 - x3) + (y4 - y3) * (y4 - y3));
+        System.out.println("Length Of Line Is :" + len1);
+
+        if (len == len1)
+        {
+            System.out.println("length of both line are equal:");
+        }
+        else if (len > len1)
+        {
+            System.out.println("length is greater then other line:");
+        }
         else
-            System.out.println("not equal");
+        {
+            System.out.println("length is less then other line:");
+        }
+
     }
     public static void main(String[] args)
     {
